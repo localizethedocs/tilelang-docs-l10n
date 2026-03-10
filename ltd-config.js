90fcce5163def49a25b926f867c23694609f50d2
ltdrobot <ltdrobot@localizethedocs.org>
2026-03-10 17:11:58 +0000

    Deploying to pages from @ localizethedocs/tilelang-docs-l10n@829636848594b326babb3b766e316291947071c3 🚀

---
+++ ltd-config.js
@@ -0,0 +1,21 @@
+"use strict";
+
+var CONFIG_OPTIONS = {
+  CONFIG_LANGUAGES: [
+    ["en-us",     "English"],
+    ["zh-cn",     "简体中文"],
+    ["zh-tw",     "繁體中文"],
+  ],
+  CONFIG_VERSIONS: [
+    ["latest",    "Development"],
+    ["stable",    "Stable Release"],
+    ["0.1",       "Release 0.1"],
+  ],
+  CONFIG_PROJECTS: [
+    ["Index",     "https://projects.localizethedocs.org"],
+    ["Crowdin",   "https://localizethedocs.crowdin.com/tilelang-docs-l10n"],
+    ["GitHub",    "https://github.com/localizethedocs/tilelang-docs-l10n"],
+    ["AtomGit",   "https://atomgit.com/localizethedocs/tilelang-docs-l10n"],
+    ["GitFlic",   "https://gitflic.ru/project/localizethedocs/tilelang-docs-l10n"],
+  ]
+};
